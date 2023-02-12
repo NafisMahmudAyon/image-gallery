deec912d8ac59b82c9c1e2bc8f594902e855484a
NafisMahmudAyon <nafismahmudayon@gmail.com>
2023-02-12 14:31:03 +0600

    2nd batch photo added

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Navbar from './Navbar';
 import ImgGallery from './ImgGallary';
 
 
-var listOfImages =[];
+// var listOfImages =[];
 
 class App extends React.Component{
     // importAll(r) {
