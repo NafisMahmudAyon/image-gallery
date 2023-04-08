e9e631f2aa77570a34758f7ff215a480e77ff78d
NafisMahmudAyon <97181505+NafisMahmudAyon@users.noreply.github.com>
2023-04-08 15:54:01 +0600

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -28,7 +28,7 @@ const Navbar = () => {
       <div class="container flex flex-wrap items-center justify-between mx-auto">
         <a href="https://flowbite.com/" class="flex items-center">
           
-          <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">NafisBD._</span>
+          <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">AI Image Gallery</span>
         </a>
         <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
           <span class="sr-only">Open main menu</span>
